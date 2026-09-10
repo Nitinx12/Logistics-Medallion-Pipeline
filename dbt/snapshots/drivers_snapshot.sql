6afb54896e52227224c4f5ccb23422eb9a7601f1
Nitin <nitin321x@gmail.com>
2026-09-10 10:46:30 +0530

    feat(dbt): update driver and vehicle snapshots

--- dbt/snapshots/drivers_snapshot.sql
+++ dbt/snapshots/drivers_snapshot.sql
@@ -5,7 +5,7 @@
     target_schema=env_var('DATABRICKS_SCHEMA_SILVER', 'silver'),
     unique_key='driver_id',
     strategy='timestamp',
-    updated_at='updated_at',
+    updated_at="CAST(updated_at AS TIMESTAMP)",
   )
 }}
 
